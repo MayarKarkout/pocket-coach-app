99f3a0ff4c42f8826e34e8545784e81f3f162a11
Mayar Karkout <karkout_mayar@hotmail.com>
2026-04-12 23:23:42 +0200

    conf

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
-  allowedDevOrigins: ["*.trycloudflare.com", "*.ts.net"],
+  allowedDevOrigins: ["*.trycloudflare.com", "*.ts.net", "goodold-ideapad-s210-touch.tail3b74a6.ts.net"],
 };
 
 export default nextConfig;
